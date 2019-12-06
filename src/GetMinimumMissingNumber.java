718c8fb5587eb0336816a8067ac58bc2e89242fe
narendra.raghuwanshi <narendra.raghuwanshi@thermofisher.com>
2019-12-06 21:13:37 +0530

    Hobby -> code

--- src/GetMinimumMissingNumber.java
+++ src/GetMinimumMissingNumber.java
@@ -1,12 +1,19 @@
 /*that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A. For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.*/
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class GetMinimumMissingNumber {
 
     private void getMinimumNumberMethod(int[] arr)
     {
-        Arrays.stream(arr).forEach(System.out::println);
+        int sum = Arrays.stream(arr).sum();
+        int sumOfN = IntStream.range(1,7).sum();
+        System.out.println(sumOfN);
+        System.out.println(sum);
+        System.out.println(sumOfN-sum);
+        //.forEach(System.out::println);
     }
     public static void main(String[] args) {
         GetMinimumMissingNumber getMinimumMissingNumber = new GetMinimumMissingNumber();
